72d49102b8f49af7c98ec43be545423132a056d1
Bruce Cantarim <bruce@cantarim.com>
2020-12-30 03:48:48 -0300

    feat(footer and loading): component logic and style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import CONST from './data/contants';
 
+import Loading from './components/Loading';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 import Carousel from './components/Carousel';
@@ -15,6 +16,7 @@ const App = () => {
 
   const [movies, setMovies] = useState();
   const [series, setSeries] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,6 +31,8 @@ const App = () => {
       );
       const seriesData = await series.json();
       setSeries(seriesData);
+
+      setLoading(false);
     };
 
     fetchData();
@@ -48,11 +52,21 @@ const App = () => {
 
   return (
     <div className='m-auto antialised font-sans bg-black text-white'>
-      <Hero {...getFeaturedMovie()} />
-      <NavBar />
-      <Carousel title='Filmes Populares' data={getMovieList()} />
-      <Carousel title='Séries Populares' data={series?.results} />
-      <Carousel title='Placeholder' />
+      {loading && (
+        <>
+          <Loading />
+          <NavBar />
+        </>
+      )}
+      {!loading && (
+        <>
+          <Hero {...getFeaturedMovie()} />
+          <NavBar />
+          <Carousel title='Filmes Populares' data={getMovieList()} />
+          <Carousel title='Séries Populares' data={series?.results} />
+          <Carousel title='Placeholder' />
+        </>
+      )}
       <Footer />
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faFacebook,
+  faFacebookSquare,
   faInstagram,
   faTwitter,
   faYoutube,
@@ -12,25 +12,67 @@ const Footer = () => {
   const today = new Date();
 
   return (
-    <footer>
-      <FontAwesomeIcon icon={faFacebook} />
-      <FontAwesomeIcon icon={faInstagram} />
-      <FontAwesomeIcon icon={faTwitter} />
-      <FontAwesomeIcon icon={faYoutube} />
-      <div>
-        <a href='#'>Idiomas e legendas</a>
-        <a href='#'>Audiodescrição</a>
-        <a href='#'>Centro de ajuda</a>
-        <a href='#'>Cartão pré-pago</a>
-        <a href='#'>Imprensa</a>
-        <a href='#'>Relação com investidores</a>
-        <a href='#'>Carreiras</a>
-        <a href='#'>Termos de uso</a>
-        <a href='#'>Privacidade</a>
-        <a href='#'>Avisos legais</a>
-        <a href='#'>Preferências de cookies</a>
-        <a href='#'>Informações corporativas</a>
-        <a href='#'>Entre em contato</a>
+    <footer className='mt-32 px-12 pb-4 text-gray-600 text-sm'>
+      <FontAwesomeIcon
+        className='mr-4 hover:text-white cursor-pointer'
+        icon={faFacebookSquare}
+        size='2x'
+      />
+      <FontAwesomeIcon
+        className='mr-4 hover:text-white cursor-pointer'
+        icon={faInstagram}
+        size='2x'
+      />
+      <FontAwesomeIcon
+        className='mr-4 hover:text-white cursor-pointer'
+        icon={faTwitter}
+        size='2x'
+      />
+      <FontAwesomeIcon
+        className='hover:text-white cursor-pointer'
+        icon={faYoutube}
+        size='2x'
+      />
+      <div className='grid grid-cols-4 gap-2 my-8'>
+        <a className='hover:text-white' href='#'>
+          Idiomas e legendas
+        </a>
+        <a className='hover:text-white' href='#'>
+          Audiodescrição
+        </a>
+        <a className='hover:text-white' href='#'>
+          Centro de ajuda
+        </a>
+        <a className='hover:text-white' href='#'>
+          Cartão pré-pago
+        </a>
+        <a className='hover:text-white' href='#'>
+          Imprensa
+        </a>
+        <a className='hover:text-white' href='#'>
+          Relação com investidores
+        </a>
+        <a className='hover:text-white' href='#'>
+          Carreiras
+        </a>
+        <a className='hover:text-white' href='#'>
+          Termos de uso
+        </a>
+        <a className='hover:text-white' href='#'>
+          Privacidade
+        </a>
+        <a className='hover:text-white' href='#'>
+          Avisos legais
+        </a>
+        <a className='hover:text-white' href='#'>
+          Preferências de cookies
+        </a>
+        <a className='hover:text-white' href='#'>
+          Informações corporativas
+        </a>
+        <a className='hover:text-white' href='#'>
+          Entre em contato
+        </a>
       </div>
       <p>© 1997 - {today.getFullYear()} Imagiflix, Inc.</p>
     </footer>
